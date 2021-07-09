cad478fae428ae433de3a8f9dbe7a27e0ea3af2f
prakashlab <hqlisu@gmail.com>
2021-07-08 18:07:30 -0700

    add gravitymachine X def

--- software/control/_def.py
+++ software/control/_def.py
@@ -203,8 +203,14 @@ DEFAULT_OBJECTIVE = '4x'
 # Gravity Machine -3 (DO NOT CHANGE)
 CAMERAS = {'DF1':{'make':'Daheng','serial':"FU0200070043", 'px_format':(4000, 3000), 'color_format': None, 'fps': 30, 'is_color':True}, 'low-mag':{'make':'Daheng','serial':"FW0200050061", 'px_format':(4000, 3000), 'color_format': None, 'fps': 30, 'is_color':False}}
 
-ROTATE_IMAGE_ANGLE = None
-FLIP_IMAGE = 'Vertical' # Horizontal, Both
+# ROTATE_IMAGE_ANGLE = None
+# FLIP_IMAGE = 'Vertical' # Horizontal, Both
+# LIQUID_LENS_FOCUS_TRACKING = False
+
+# Gravity Machine X
+CAMERAS = {'DF1':{'make':'Daheng','serial':"FU0200080047", 'px_format':(4000,3000), 'color_format': 'GRAY8', 'fps': 30,'is_color':True}}
+ROTATE_IMAGE_ANGLE = 90
+FLIP_IMAGE = 'Both' # Horizontal, Both
 LIQUID_LENS_FOCUS_TRACKING = False
 
 # stage movement sign - it depends on camera orientation and stage configuration
